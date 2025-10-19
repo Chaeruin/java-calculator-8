448d0bcf19d386fb09c32eff8b6edf7a953bab2b
Chaeruin <cms3604@naver.com>
2025-10-19 14:23:55 +0900

    feat: CalculatorController 구현

---
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -0,0 +1,32 @@
+package calculator.controller;
+
+import calculator.service.Calculator;
+import calculator.util.BasicDelimiterParser;
+import calculator.util.CustomDelimiterParser;
+import calculator.util.DelimiterParser;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
+public class CalculatorController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final Calculator calculator;
+    private DelimiterParser delimiterParser;
+
+    public CalculatorController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.calculator = new Calculator();
+        this.delimiterParser = new BasicDelimiterParser();
+    }
+
+    public void run() {
+        String input = inputView.getInput();
+        if (delimiterParser.isCustomDelimiter(input)) {
+            delimiterParser = new CustomDelimiterParser();
+        }
+        double doubleResult = calculator.calculate(delimiterParser.splits(input));
+        outputView.printCalculateResult(calculator.getResult(doubleResult));
+    }
+}
