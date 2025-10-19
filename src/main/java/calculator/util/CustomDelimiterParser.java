1ae943b184b18ed374616b25d9ce68ff7d214af0
Chaeruin <cms3604@naver.com>
2025-10-19 13:35:34 +0900

    feat: 커스텀 입력 구분자와 다른 구분자 사용 예외 처리 구현

--- src/main/java/calculator/util/CustomDelimiterParser.java
+++ src/main/java/calculator/util/CustomDelimiterParser.java
@@ -27,7 +27,21 @@ public class CustomDelimiterParser implements DelimiterParser {
             throw new IllegalArgumentException(ErrorCode.INVALID_POSITION_OF_DELIMITER.getErrorName());
         }
 
+        if (!isFitDelimiter(otherNumbers, customDelimiter)) {
+            throw new IllegalArgumentException(ErrorCode.DIFFERENT_CUSTOM_DELIMITER_INPUT.getErrorName());
+        }
+
         return otherNumbers.split(customDelimiter);
+    }
+
+    private boolean isFitDelimiter(String otherNumbers, String delimiter) {
+        char[] numbers = otherNumbers.toCharArray();
+        for (int i = 1; i < numbers.length; i += 2) {
+            if (numbers[i] != delimiter.charAt(0)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private boolean isNotStartOrEndWithNumbers(String otherNumbers, String customDelimiter) {
