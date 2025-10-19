2b0b44cdc2aa929f5d555798d02fd300c3473312
Chaeruin <cms3604@naver.com>
2025-10-19 16:16:25 +0900

    test: 정상 처리 및 예외 처리에 대한 테스트코드 작성

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
@@ -29,5 +30,150 @@ class ApplicationTest extends NsTest {
         Application.main(new String[]{});
     }
 
+    // 커스텀 테스트 //
+    // 정상 경우 테스트
+    @Test
+    @DisplayName("빈 값 입력 테스트")
+    void test0() {
+        assertSimpleTest(() -> {
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
 
+    @Test
+    @DisplayName("정상 기본 구분자 입력 테스트")
+    void test1() {
+        assertSimpleTest(() -> {
+            run("1,1:1");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+
+    @Test
+    @DisplayName("정상 커스텀 구분자 입력 테스트")
+    void test2() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2;3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    @DisplayName("정상 커스텀 구분자 입력 테스트2")
+    void test3() {
+        assertSimpleTest(() -> {
+            run("//.\\n1.2.3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    // 예외 처리 테스트
+    @Test
+    @DisplayName("숫자 입력이 아닌 경우 예외 (기본 구분자)")
+    void test4() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("a,2:b"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("숫자 입력이 아닌 경우 예외 (커스텀 구분자)")
+    void test5() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\\na;2;b"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("양수 입력이 아닌 경우(음수 입력) 예외 (기본 구분자)")
+    void test6() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("-1,2:3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("양수 입력이 아닌 경우(음수 입력) 예외 (커스텀 구분자)")
+    void test7() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\\n-1;2;3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("기본 구분자 경우에서의 커스텀 구분자 임의 사용 예외")
+    void test8() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1;2:3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("입력했던 커스텀 구분자가 아닌 다른 구분자 사용")
+    void test9() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\\n1'2;3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("구분자 연속 등장 예외 (기본 구분자)")
+    void test10() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,,2:3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("구분자 연속 등장 예외 (커스텀 구분자)")
+    void test11() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\\n1;;2;3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("구분자 위치 부정확으로 인한 예외 (기본 구분자 위치 앞)")
+    void test12() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(":1:2:3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("구분자 위치 부정확으로 인한 예외 (기본 구분자 위치 뒤)")
+    void test13() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1:2:3:"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("구분자 위치 부정확으로 인한 예외 (커스텀 구분자 위치 앞)")
+    void test14() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\\n;1;2;3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("구분자 위치 부정확으로 인한 예외 (커스텀 구분자 위치 뒤)")
+    void test15() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\\n1;2;3;"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 }
